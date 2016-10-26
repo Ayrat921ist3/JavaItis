9eef40bc59e33414a56652e4adf1d2709faee242
Ayrat <airat116russia@gmail.com>
2016-10-26 18:15:56 +0300

    adding filter

--- SimpleAuthService/src/main/java/ru/khannanovayrat/filter/ListFilter.java
+++ SimpleAuthService/src/main/java/ru/khannanovayrat/filter/ListFilter.java
@@ -8,7 +8,7 @@ import java.io.IOException;
  */
 public class ListFilter implements Filter {
     public void init(FilterConfig filterConfig) throws ServletException {
-        
+
     }
 
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
